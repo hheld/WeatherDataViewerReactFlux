816b7179ffb65bac7df5d01e38e64911648d95b3
Harald Held <harald.held@gmail.com>
2015-03-29 16:18:46 +0200

    Moved update button to own component.

---
+++ js/components/Button.js
@@ -0,0 +1,46 @@
+/* jshint node: true */
+
+var React      = require('react'),
+    Radium     = require('radium');
+
+var StyleResolverMixin = Radium.StyleResolverMixin,
+    BrowserStateMixin = Radium.BrowserStateMixin;
+
+var buttonStyle = {
+    float: 'left',
+    borderRadius: 3,
+    border: '1px solid #0b0e07',
+    display: 'inline-block',
+    cursor: 'pointer',
+    color: '#ffffff',
+    fontFamily: 'arial',
+    fontSize: 15,
+    padding: '5px 15px',
+    textDecoration: 'none',
+    textShadow: '0px 1px 0px #263666',
+    background: 'linear-gradient(to bottom, #2dabf9 5%, #0688fa 100%)',
+    backgroundColor: '#2dabf9',
+
+    states: [{
+        hover: {
+            background:'linear-gradient(to bottom, #0688fa 5%, #2dabf9 100%)',
+            backgroundColor: '#0688fa'
+        },
+        active: {
+            position: 'relative',
+            top: 1
+        }
+    }]
+};
+
+var Button = React.createClass({
+    mixins: [ StyleResolverMixin, BrowserStateMixin ],
+
+    render: function() {
+        return(
+                <button {...this.getBrowserStateEvents()} style={this.buildStyles(buttonStyle)} onClick={this.props.clickHandler}>Update</button>
+        );
+    }
+});
+
+module.exports = Button;
--- js/components/DataPanel.js
+++ js/components/DataPanel.js
@@ -3,11 +3,8 @@
 var React      = require('react'),
     AppStore   = require('../stores/AppStore'),
     ApiActions = require('../actions/ApiActions.js'),
-    DataPlot   = require('./DataPlot.js'),
-    Radium     = require('radium');
-
-var StyleResolverMixin = Radium.StyleResolverMixin,
-    BrowserStateMixin = Radium.BrowserStateMixin;
+    DataPlot   = require('./DataPlot'),
+    Button     = require('./Button');
 
 var plotContainerStyle = {
     width: '80%',
@@ -15,36 +12,7 @@ var plotContainerStyle = {
     float: 'right'
 };
 
-var updateButtonStyle = {
-    float: 'left',
-    borderRadius: 3,
-    border: '1px solid #0b0e07',
-    display: 'inline-block',
-    cursor: 'pointer',
-    color: '#ffffff',
-    fontFamily: 'arial',
-    fontSize: 15,
-    padding: '5px 15px',
-    textDecoration: 'none',
-    textShadow: '0px 1px 0px #263666',
-    background: 'linear-gradient(to bottom, #2dabf9 5%, #0688fa 100%)',
-    backgroundColor: '#2dabf9',
-
-    states: [{
-        hover: {
-            background:'linear-gradient(to bottom, #0688fa 5%, #2dabf9 100%)',
-            backgroundColor: '#0688fa'
-        },
-        active: {
-            position: 'relative',
-            top: 1
-        }
-    }]
-};
-
 var DataPanel = React.createClass({
-    mixins: [ StyleResolverMixin, BrowserStateMixin ],
-
     getInitialState: function() {
         return this._getAppState();
     },
@@ -81,7 +49,7 @@ var DataPanel = React.createClass({
                 <div style={plotContainerStyle}>
                     <DataPlot {...this.props} data={this.state.data}></DataPlot>
                 </div>
-                <button {...this.getBrowserStateEvents()} style={this.buildStyles(updateButtonStyle)} onClick={this._onUpdateButtonClicked}>Update</button>
+                <Button clickHandler={this._onUpdateButtonClicked}>Update</Button>
             </div>
         );
     }
