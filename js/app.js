6876920834d223ffe38b0ee8405b5b2f59c0c59c
Harald Held <harald.held@gmail.com>
2015-03-31 10:52:19 +0200

    Added wind gust direction frequency plot.

--- js/app.js
+++ js/app.js
@@ -11,15 +11,16 @@ var from = AppStore.getFrom();
 var to = AppStore.getTo();
 
 var dataNames = [
-    { datum: 'inTemp',      displayText: 'Temperature inside',       stat: 'Avg.', unit: '°C',   conversionFunc: unitConversionFunctions.F2C },
-    { datum: 'outTemp',     displayText: 'Temperature outside',      stat: 'Avg.', unit: '°C',   conversionFunc: unitConversionFunctions.F2C },
-    { datum: 'inHumidity',  displayText: 'Humidity inside',          stat: 'Avg.', unit: '%' },
-    { datum: 'outHumidity', displayText: 'Humidity outside',         stat: 'Avg.', unit: '%' },
-    { datum: 'rain',        displayText: 'Rain',                     stat: 'Sum',  unit: 'mm',   conversionFunc: unitConversionFunctions.in2mm },
-    { datum: 'barometer',   displayText: 'Barometer',                stat: 'Avg.', unit: 'mbar', conversionFunc: unitConversionFunctions.inHg2mbar },
-    { datum: 'windSpeed',   displayText: 'Wind speed',               stat: 'Avg.', unit: 'm/s',  conversionFunc: unitConversionFunctions.milesPerHour2mPerSec },
-    { datum: 'windGust',    displayText: 'Wind gust speed',          stat: 'Avg.', unit: 'm/s',  conversionFunc: unitConversionFunctions.milesPerHour2mPerSec },
-    { datum: 'windDir',     displayText: 'Wind direction frequency', specialWidget: 'windRose' }
+    { datum: 'inTemp',      displayText: 'Temperature inside',            stat: 'Avg.', unit: '°C',   conversionFunc: unitConversionFunctions.F2C },
+    { datum: 'outTemp',     displayText: 'Temperature outside',           stat: 'Avg.', unit: '°C',   conversionFunc: unitConversionFunctions.F2C },
+    { datum: 'inHumidity',  displayText: 'Humidity inside',               stat: 'Avg.', unit: '%' },
+    { datum: 'outHumidity', displayText: 'Humidity outside',              stat: 'Avg.', unit: '%' },
+    { datum: 'rain',        displayText: 'Rain',                          stat: 'Sum',  unit: 'mm',   conversionFunc: unitConversionFunctions.in2mm },
+    { datum: 'barometer',   displayText: 'Barometer',                     stat: 'Avg.', unit: 'mbar', conversionFunc: unitConversionFunctions.inHg2mbar },
+    { datum: 'windSpeed',   displayText: 'Wind speed',                    stat: 'Avg.', unit: 'm/s',  conversionFunc: unitConversionFunctions.milesPerHour2mPerSec },
+    { datum: 'windGust',    displayText: 'Wind gust speed',               stat: 'Avg.', unit: 'm/s',  conversionFunc: unitConversionFunctions.milesPerHour2mPerSec },
+    { datum: 'windDir',     displayText: 'Wind direction frequency',      specialWidget: 'windRose' },
+    { datum: 'windGustDir', displayText: 'Wind gust direction frequency', specialWidget: 'windRose' }
 ];
 
 var panels = dataNames.map(function(datum, i) {
